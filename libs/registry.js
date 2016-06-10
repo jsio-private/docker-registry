8587ceed3a0228fa58ba20c71d8846b5bdb5d387
yofreke <sparkyjoe94@aim.com>
2016-06-10 05:12:30 -0700

    add better support for basic auth

--- libs/registry.js
+++ libs/registry.js
@@ -4,11 +4,16 @@ var apis = require('./apis');
 module.exports = registry;
 
 
+var encodeBasic = function (s) {
+  return new Buffer(s).toString('base64');
+};
+
+
 /**
  * @param  {String} host
  * @param  {Object} [opts]
- * @param  {Object} [opts.auth]
- * @param  {Object} [opts.apiVersion]
+ * @param  {Object} [opts.auth] - Either an object with `username` and `password`, or a string `username:password`.  If none is provided, the docker config will be checked.
+ * @param  {String} [opts.apiVersion=v2]
  */
 var registry = function (host, opts) {
   if (!host) {
@@ -19,9 +24,23 @@ var registry = function (host, opts) {
 
   var rules = {};
   if (opts.auth) {
+    var encoded;
+    var authType = typeof opts.auth;
+    if (authType === 'object') {
+      encoded = encodeBasic(opts.auth.username + ':' + opts.auth.password);
+    } else if (authType === 'string') {
+      if (opts.auth.indexOf(':') >= 0) {
+        encoded = encodeBasic(opts.auth);
+      } else {
+        encoded = opts.auth;
+      }
+    } else {
+      throw new Error('Unknown auth type: ' + authType);
+    }
+
     rules.all = {
       headers: {
-        Authorization: 'Basic ' + new Buffer(opts.auth.username + ':' + opts.auth.password).toString('base64')
+        Authorization: 'Basic ' + encoded
       }
     }
   }
